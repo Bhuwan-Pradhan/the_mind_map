dca76dce7d7556604f7728419d4212912cab952c
Yash Arya <yasharya82003@gmail.com>
2024-03-12 01:13:53 +0530

    Case Container Page styled

--- src/components/CaseContainerHome.jsx
+++ src/components/CaseContainerHome.jsx
@@ -25,6 +25,52 @@ export default function CaseContainerHome() {
                 </button>
             </div>
         </div>
+        <div className="CaseContainerMainOuter">
+            <div className="CaseBrief">
+                <div className="CaseNameDiv">
+                    <p>Choori ho gayi hai</p>
+                </div>
+                <div className="CaseDescriptionDiv">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum corporis amet, atque labore quod maxime fugit iste in quisquam, molestiae tempore repudiandae! Quaerat vel assumenda amet, quod sed provident aliquid.....</p>
+                </div>
+            </div>
+
+            <div className="CaseImage">
+                <img src="https://www.baezlawfirm.com/wp-content/uploads/2021/05/shutterstock_644215882-200x200.png" alt="Theft Image" />
+            </div>
+
+            <div className="AddNewDetailButtons">
+                <button class="AddNewDetailButton">
+                    Add Clue
+                </button>
+                <button class="AddNewDetailButton">
+                    Add Person
+                </button>
+            </div>
+        </div>
+        <div className="CaseContainerMainOuter">
+            <div className="CaseBrief">
+                <div className="CaseNameDiv">
+                    <p>Bhopal Gas Trajedy of 1984, Death of 3000 People</p>
+                </div>
+                <div className="CaseDescriptionDiv">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum corporis amet, atque labore quod maxime fugit iste in quisquam, molestiae tempore repudiandae! Quaerat vel assumenda amet, quod sed provident aliquid.....</p>
+                </div>
+            </div>
+
+            <div className="CaseImage">
+                <img src="https://www.baezlawfirm.com/wp-content/uploads/2021/05/shutterstock_644215882-200x200.png" alt="Theft Image" />
+            </div>
+
+            <div className="AddNewDetailButtons">
+                <button class="AddNewDetailButton">
+                    Add Clue
+                </button>
+                <button class="AddNewDetailButton">
+                    Add Person
+                </button>
+            </div>
+        </div>
         </div>
     );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import NavBar from "../components/NavBar";
-
+import CaseContainerHome from "../components/CaseContainerHome"
 export default function HomePage(){
     return(
         <div>
             <NavBar />
+            <CaseContainerHome/>
         </div>
     );
 }
