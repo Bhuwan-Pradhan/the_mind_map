b08ff810a1bc254ffb131d9efd33bd8fa2da8dc9
Prem Roshan <premroshan209@gmail.com>
2024-03-12 19:55:15 +0530

    AddCase fixed bugs

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import NavBar from "../components/NavBar";
 import CaseContainerHome from "../components/CaseContainerHome"
+import AddCase from "../components/AddCase";
+import AddClue from "../components/AddClue";
+import AddPerson from "../components/AddPerson";
 export default function HomePage(){
     return(
         <div>
             <NavBar />
-            <CaseContainerHome/>
+            <AddPerson/>
         </div>
     );
 }
