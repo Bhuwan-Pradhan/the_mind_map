36ead5c0cf9d8953c956d4d6a1a8a531678a059d
Yash Arya <yasharya82003@gmail.com>
2024-03-16 16:07:07 +0530

    Nave baar responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,15 +9,18 @@ export default function NavBar() {
     const navigate = useNavigate();
     const { user } = useSelector((state) => state.auth);
     return (
-        <div class="navbar">
+        <div className="navbar">
+            <div className="imgContainerNav">
             <img src={logo} alt="TheMindMap" />
+            <span class="leftWelcome">Welcome, <strong>{user.name}</strong></span>
+            </div>
             {/* <form>
             <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="srch" aria-hidden="true"><path d="m19 19-3.5-3.5">
                 </path><circle cx="11" cy="11" r="6"></circle></svg>
                 <input type="text" class="searchBar" placeholder="Search Clue ..."/>
             </form> */}
-            <div class="navbar-right">
-                <span>Welcome, <strong>{user.name}</strong></span>
+            <div className="navbar-right">
+                <span className="rightWelcome">Welcome, <strong>{user.name}</strong></span>
                 <div className="Logout">
                     <button
                         onClick={() => {
