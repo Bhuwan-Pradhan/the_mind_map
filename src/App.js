e0363a50bfa01e97d9e18f22907c41670a795a0f
Yash Arya <yasharya82003@gmail.com>
2024-03-18 14:47:02 +0530

    Dark mode enabled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import './App.css';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -17,9 +18,61 @@ import UpdatePerson from "./components/UpdatePerson";
 
 function App() {
   const { token } = useSelector((state) => state.auth);
+  let st=0;
+  function changetheme(){
+    if(st===0){
+      
+      // alert(document.body);
+      document.body.style.backgroundColor="#0F172A";
+      // setTimeout(() => {
+      //   changethm(1);
+      // }, 2000);
+      let crds=document.getElementsByClassName("card");
+      for (let i = 0; i < crds.length; i++) {
+        document.getElementsByClassName("inside-page__text")[i].style.color="#f5f5f5";
+        document.getElementsByClassName("card-front")[i].style.backgroundColor="#1E293B";
+        document.getElementsByClassName("card-back")[i].style.backgroundColor="#1E293B";
+        document.getElementsByClassName("inside-page")[i].style.backgroundColor="#1E293B";
+        document.getElementsByClassName("card-front__text-view")[i].style.color="#f5f5f5";
+        document.getElementsByClassName("inside-page__heading--city")[i].style.color="#f5f5f5";
+        document.getElementsByClassName("inside-page__btn--city")[2*i].style.color="#e5e5e5";
+        document.getElementsByClassName("inside-page__btn--city")[2*i+1].style.color="#e5e5e5";
+        document.getElementsByClassName("inside-page__btn--city")[2*i].style.backgroundColor="#21434E";
+        document.getElementsByClassName("inside-page__btn--city")[2*i+1].style.backgroundColor="#21434E";
+        document.getElementsByClassName("inside-page__btn--city")[2*i].style.border="1px solid #21439f";
+        document.getElementsByClassName("inside-page__btn--city")[2*i+1].style.border="1px solid #21439f";
+      }
+      st=1;
+      // changethm(1);
+      // console.log(document.getElementsByClassName("card-front")[0]);
+    }else{
+      document.body.style.backgroundColor="#f5f5f5";
+      let crds=document.getElementsByClassName("card");
+      for (let i = 0; i < crds.length; i++) {
+        document.getElementsByClassName("inside-page__text")[i].style.color="#333";
+      document.getElementsByClassName("card-front")[i].style.backgroundColor="#ebc489";
+        document.getElementsByClassName("card-back")[i].style.backgroundColor="#ebc28a";
+        document.getElementsByClassName("inside-page")[i].style.backgroundColor="#ebc489";
+        document.getElementsByClassName("card-front__text-view")[i].style.color="#584e53";
+        document.getElementsByClassName("inside-page__heading--city")[i].style.color="#584e53";
+      }
+      for(let i=0;i<2*crds.length;i++){
+        document.getElementsByClassName("inside-page__btn--city")[i].style.color="#514b4e";
+        document.getElementsByClassName("inside-page__btn--city")[i].style.backgroundColor="transparent";
+        document.getElementsByClassName("inside-page__btn--city")[i].style.border="3px solid #6f676b";
+      }
+      st=0;
+      // changethm(0);
+      
+    }
+  }
+//   useEffect(() => {
+//     changetheme();
+// }, []);
   if (token) {
     return (
       <div className="App">
+        <button className="themebutton" onClick={changetheme}>Change</button>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/signup" element={<Signup />} />
